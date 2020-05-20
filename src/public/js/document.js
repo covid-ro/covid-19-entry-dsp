0dd90b45d45d900399015dbd160012d3cf81bac9
abau <alexandru.bau@gmail.com>
2020-05-20 20:38:39 +0300

    feat: added new declaration
    Refs #69 #70

--- src/public/js/document.js
+++ src/public/js/document.js
@@ -76,208 +76,116 @@ Document.prototype.getName = function(data) {
 
 Document.prototype.draw = function () {
 
-    this.doc.rect(10, 10, 190, 10);
-
     this.doc.setFontStyle('bold');
     this.doc.setFontSize(12);
     this.doc.text(this.__('declaration'), 105, 16, { align: 'center' });
 
     this.reset();
 
-    // left column
-    this.doc.text(this.__('last_name'), 10, 30);
-    this.doc.line(22, 31, 100, 31);
+    // first rectangle
+    this.doc.rect(10, 45, 190, 16);
 
-    this.doc.text(this.__('first_name'), 10, 35);
-    this.doc.line(25, 36, 100, 36);
+    this.doc.text(this.__('recommendation'), 12, 50);
 
-    this.doc.text(this.__('gender'), 10, 40);
-    this.doc.rect(55, 37.2, 3, 3);
-    this.doc.text('M', 60, 40);
-    this.doc.rect(70, 37.2, 3, 3);
-    this.doc.text('F', 75, 40);
+    this.doc.rect(12, 53, 3, 3);
+    this.doc.text(this.__('send_to_hospital'), 17, 55.5);
 
-    this.doc.text(this.__('passport'), 10, 45);
-    this.doc.text(this.__('series'), 35, 45);
-    this.doc.line(45, 46, 60, 46);
-    this.doc.text(this.__('number'), 67, 45);
-    this.doc.line(80, 46, 100, 46);
+    this.doc.rect(52, 53, 3, 3);
+    this.doc.text(this.__('institutionalized_quarantine'), 57, 55.5);
 
-    this.doc.text(this.__('date_of_birth'), 10, 50);
-    this.doc.line(49, 51, 100, 51);
+    this.doc.rect(102, 53, 3, 3);
+    this.doc.text(this.__('home_isolation'), 107, 55.5);
 
-    this.doc.text(this.__('date_of_arrival_in_romania'), 10, 55);
-    this.doc.line(49, 56, 100, 56);
+    this.doc.line(160, 45, 160, 61);
+    this.doc.text(this.__('agent_signature'), 162, 50);
 
-    // right column
-    this.doc.text(this.__('country_leave'), 105, 30);
-    this.doc.line(140, 31, 200, 31);
+    // second rectangle
+    this.doc.rect(10, 65, 190, 20);
 
-    this.doc.text(this.__('locality_leave'), 105, 35);
-    this.doc.line(122, 36, 200, 36);
+    this.doc.text(this.__('last_name'), 12, 70);
+    this.doc.line(28, 70.5, 80, 70.5);
 
-    this.doc.text(this.__('date_leave'), 105, 40);
-    this.doc.line(115, 41, 200, 41);
+    this.doc.text(this.__('first_name'), 90, 70);
+    this.doc.line(106, 70.5, 197, 70.5);
 
-    // table
-    this.doc.text(this.__('estimate_stay'), 10, 65);
+    this.doc.text(this.__('identity_number'), 12, 75);
+    this.doc.line(28, 75.5, 84, 75.5);
 
-    this.doc.setFontStyle('bold');
+    this.doc.text(this.__('date_of_birth'), 92, 75);
 
-    // table - head
-    this.doc.text(this.__('current_number'), 20, 72, { align: 'center' });
-    this.doc.text(this.__('location_town'), 52, 72, { align: 'center' });
-    this.doc.text(this.__('date_of_arrival'), 87, 72, { align: 'center' });
-    this.doc.text(this.__('date_of_leave'), 113, 72, { align: 'center' });
-    this.doc.text(this.__('full_address'), 160, 72, { align: 'center' });
+    this.doc.text(this.__('year'), 130, 75);
+    this.doc.line(115, 75.5, 129, 75.5);
 
-    this.reset();
+    this.doc.text(this.__('month'), 160, 75);
+    this.doc.line(145, 75.5, 159, 75.5);
 
-    // table - horizontal lines
-    this.doc.line(10, 74, 200, 74);
-    this.doc.line(10, 84, 200, 84);
-    this.doc.line(10, 94, 200, 94);
+    this.doc.text(this.__('day'), 190, 75);
+    this.doc.line(175, 75.5, 189, 75.5);
+
+    this.doc.text(this.__('country_departure'), 12, 80);
+    this.doc.line(45, 80.5, 90, 80.5);
+
+    // free text
+    this.doc.text(this.__('i_declare'), 10, 100);
+
+    this.doc.text(this.__('first_question'), 10, 105, { maxWidth: 190 });
+
+    this.doc.text(this.__('second_question'), 10, 115, { maxWidth: 190 });
+    this.doc.line(58, 119.8, 200, 119.8);
+
+    this.doc.text(this.__('agree_gdpr'), 10, 125, { maxWidth: 190 });
+
+    this.doc.text(this.__('agree_lies'), 10, 130, { maxWidth: 190 });
 
     // contact
-    this.doc.text(this.__('contact_me_at'), 10, 110);
-    this.doc.text(this.__('phone_number'), 10, 115);
-    this.doc.line(24, 116, 60, 116);
-    this.doc.text(this.__('email'), 65, 115);
-    this.doc.line(76, 116, 140, 116);
+    this.doc.text(this.__('contact_at'), 10, 140);
 
-    // questions
-    let quesiton1 = this.__('has_visited');
-    this.doc.text(quesiton1, 10, 120, { maxWidth: 190 });
-    this.doc.rect(10, 122.2, 3, 3);
-    this.doc.text(this.__('yes'), 15, 125);
-    this.doc.rect(25, 122.2, 3, 3);
-    this.doc.text(this.__('no'), 30, 125);
+    this.doc.text(this.__('phone'), 10, 145);
+    this.doc.line(35, 145.5, 70, 145.5);
 
-    let quesiton2 = this.__('has_contacted');
-    this.doc.text(quesiton2, 10, 130, { maxWidth: 190 });
-    this.doc.rect(10, 137.2, 3, 3);
-    this.doc.text(this.__('yes'), 15, 140);
-    this.doc.rect(25, 137.2, 3, 3);
-    this.doc.text(this.__('no'), 30, 140);
-
-    let quesiton3 = this.__('is_hospitalized');
-    this.doc.text(quesiton3, 10, 145, { maxWidth: 190 });
-    this.doc.rect(10, 147.2, 3, 3);
-    this.doc.text(this.__('yes'), 15, 150);
-    this.doc.rect(25, 147.2, 3, 3);
-    this.doc.text(this.__('no'), 30, 150);
-
-    let quesiton4 = this.__('has_symptoms');
-    this.doc.text(quesiton4, 10, 155, { maxWidth: 190 });
-
-    this.doc.text('- ' + this.__('fever'), 10, 160);
-    this.doc.rect(50, 157.2, 3, 3);
-    this.doc.text(this.__('yes'), 55, 160);
-    this.doc.rect(65, 157.2, 3, 3);
-    this.doc.text(this.__('no'), 70, 160);
-
-    this.doc.text('- ' + this.__('difficulty_swallow'), 10, 165);
-    this.doc.rect(50, 162.2, 3, 3);
-    this.doc.text(this.__('yes'), 55, 165);
-    this.doc.rect(65, 162.2, 3, 3);
-    this.doc.text(this.__('no'), 70, 165);
-
-    this.doc.text('- ' + this.__('difficulty_breath'), 10, 170);
-    this.doc.rect(50, 167.2, 3, 3);
-    this.doc.text(this.__('yes'), 55, 170);
-    this.doc.rect(65, 167.2, 3, 3);
-    this.doc.text(this.__('no'), 70, 170);
-
-    this.doc.text('- ' + this.__('intense_cough'), 10, 175);
-    this.doc.rect(50, 172.2, 3, 3);
-    this.doc.text(this.__('yes'), 55, 175);
-    this.doc.rect(65, 172.2, 3, 3);
-    this.doc.text(this.__('no'), 70, 175);
-
-    // bigtext
-    let text1 = this.__('important_notice');
-    this.doc.text(text1, 10, 180, { maxWidth: 190 });
-
-    let text2 = this.__('agreement_1');
-    this.doc.text(text2, 10, 215, { maxWidth: 190 });
-
-    let text3 = this.__('agreement_2');
-    this.doc.text(text3, 10, 225, { maxWidth: 190 });
-
-    let text4 = this.__('agreement_3');
-    this.doc.text(text4, 10, 245, { maxWidth: 190 });
-
-    let text5 = this.__('agreement_4');
-    this.doc.text(text5, 10, 265, { maxWidth: 190 });
-
-    // date
-    this.doc.text(this.__('date_place'), 50, 275);
-    this.doc.text(this.__('signature'), 140, 275);
+    // footer
+    this.doc.text(this.__('signature'), 30, 200);
+    this.doc.text(this.__('date'), 167, 200);
 };
 
 Document.prototype.fill = function (data) {
 
-    // top
-    this.doc.text(data.locale === 'en' ? 'EN' : 'RO', 190, 16);
+    // first rectangle
+    if (data.measure.hospital) {
+        this.doc.text('X', 12, 55.5);
+    }
+    if (data.measure.quarantine) {
+        this.doc.text('X', 52, 55.5);
+    }
+    if (data.measure.isolation) {
+        this.doc.text('X', 102, 55.5);
+    }
+
+    // second rectangle
+    this.doc.text(data.lastName, 55, 70, { align: 'center' });
+    this.doc.text(data.firstName, 150, 70, { align: 'center' });
+    this.doc.text(data.idCardNumber, 55, 75, { align: 'center' });
+    this.doc.text(data.dateOfBirth.year, 122, 75, { align: 'center' });
+    this.doc.text(data.dateOfBirth.month, 152, 75, { align: 'center' });
+    this.doc.text(data.dateOfBirth.day, 182, 75, { align: 'center' });
+    this.doc.text(data.countryDeparture, 68, 80, { align: 'center' });
+
+    // free text
+    this.doc.text(data.destinationAddress, 60, 119.2);
+    this.doc.text(data.phoneNumber, 52, 145, { align: 'center' });
+
+    // footer
+    this.doc.text(data.documentDate, 170, 205, { align: 'center' });
 
 
-    // left column
-    this.doc.text(data.lastName, 65, 30, { align: 'center' });
-    this.doc.text(data.firstName, 65, 35, { align: 'center' });
-    this.doc.text('X', data.sex === 'M' ? 56.5 : 71.5, 40, { align: 'center' });
-    this.doc.text(data.idCardSeries, 53, 45, { align: 'center' });
-    this.doc.text(data.idCardNumber, 90, 45, { align: 'center' });
-    this.doc.text(data.birthday, 75, 50, { align: 'center' });
-    this.doc.text(data.dateArrival, 75, 55, { align: 'center' });
-
-    // right column
-    this.doc.text(data.countryLeave, 170, 30, { align: 'center' });
-    this.doc.text(data.localityLeave, 170, 35, { align: 'center' });
-    this.doc.text(data.dateLeave, 170, 40, { align: 'center' });
-
-    // table
-    data.addresses.forEach(function (address, index) {
-
-        let height = (index+1)*10;
-
-        this.doc.text(index+1+'', 20, 70 + height, { align: 'center' });
-        this.doc.text(address.locality, 52, 70 + height, { align: 'center' });
-        this.doc.text(address.dateArrival, 87, 70 + height, { align: 'center' });
-        this.doc.text(address.dateLeave, 113, 70 + height, { align: 'center' });
-        this.doc.text(address.fullAddress, 160, 68 + height, { align: 'center', maxWidth: 65 });
-
-    }.bind(this));
-
-    // contact
-    this.doc.text(data.phoneNumber, 43, 115, { align: 'center' });
-    this.doc.text(data.emailAddress, 106, 115, { align: 'center' });
-
-    // questions
-    this.doc.text('X', data.answers.hasVisited ? 10 : 25, 125);
-    this.doc.text('X', data.answers.hasContacted ? 10 : 25, 140);
-    this.doc.text('X', data.answers.isHospitalized ? 10 : 25, 150);
-    this.doc.text('X', data.answers.hasFever ? 50 : 65, 160);
-    this.doc.text('X', data.answers.hasDifficultySwallow ? 50 : 65, 165);
-    this.doc.text('X', data.answers.hasDifficultyBreath ? 50 : 65, 170);
-    this.doc.text('X', data.answers.hasIntenseCough ? 50 : 65, 175);
-
-    // bigtext
-    this.doc.text(data.organization, data.locale === 'en' ? 97 : 159, 192, { align: 'center' });
-    this.doc.text(data.visitedCountries.join(', '), data.locale === 'en' ? 135 : 125, 233, { align: 'center' });
-    this.doc.text(data.borderCrossingPoint, data.locale === 'en' ? 50 : 75, 241, { align: 'center' });
-    this.doc.text(data.destination, data.locale === 'en' ? 122 : 145, 249, { align: 'center' });
-    this.doc.text(data.vehicle, data.locale === 'en' ? 56 : 103, 253, { align: 'center' });
-    this.doc.text(data.route, 100, 257, { align: 'center' });
-    this.doc.text(data.documentDate + ', ' + data.documentLocality, 59, 280, { align: 'center' });
 };
 
 Document.prototype.addSignature = function (signature) {
-    if(signature.length > 0) {
-        this.doc.addImage(signature, 'PNG', 155, 260, 40, 30);
-    }
+
+    this.doc.addImage(signature, 'PNG', 20, 205, 40, 30);
 };
 
 Document.prototype.addQrCode = function (qrcode) {
 
-    this.doc.addImage(qrcode, 'PNG', 185, 45, 15, 15);
+    this.doc.addImage(qrcode, 'PNG', 185, 8, 15, 15);
 };
