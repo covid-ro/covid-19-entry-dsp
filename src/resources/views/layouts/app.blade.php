d6e0e6c8662173feb1656a84627c5189738d0851
Marius Ghitoiu <marius.ghitoiu@citizennext.ro>
2020-05-23 02:02:11 +0300

    Fix small translation stuff.

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -50,7 +50,7 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ env('WEB_FORM_URL', '#') }}">{{ __('Complete declaration') }}</a>
+                            <a class="nav-link" href="{{ env('WEB_FORM_URL', '#') }}" target="_blank" rel="noopener">{{ __('app.Complete declaration') }}</a>
                         </li>
                         @guest
                             <li class="nav-item">
