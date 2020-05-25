f281a7df80d1233cad01a96fa844a78a4edee655
tudor <tudor.rusu@hotmail.com>
2020-05-25 22:54:49 +0200

    fix(search): fix prevent enter press for submit

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -135,6 +135,15 @@
             @endif
         </div>
         <script type="text/javascript">
+            $(document).ready(function() {
+                $(window).keydown(function(event){
+                    if(event.keyCode == 13) {
+                        event.preventDefault();
+                        return false;
+                    }
+                });
+            });
+
             $.ajaxSetup({
                 headers: {
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
